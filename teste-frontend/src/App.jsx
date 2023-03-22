cc842351cd89524be99918385ef5b73800ffba9d
AdsonVilla <adson23_@hotmail.com>
2023-03-22 16:26:31 -0300

    feat: criar rotas com react-router-dom

--- teste-frontend/src/App.jsx
+++ teste-frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import { RoutesApp } from "./routes";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="container">
+      <RoutesApp />
     </div>
   );
 }
---
+++ teste-frontend/src/pages/Comments/index.jsx
@@ -0,0 +1,11 @@
+import "./comments.css";
+
+function Comments() {
+  return (
+    <div>
+      <h1>Comentários</h1>
+    </div>
+  );
+}
+
+export default Comments;
