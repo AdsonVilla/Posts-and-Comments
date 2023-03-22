dfc3cb669602852c96a55c81c45b260cbf31c745
AdsonVilla <adson23_@hotmail.com>
2023-03-22 19:18:13 -0300

    perf: Utilizar a hooks e axios para consumir a API

--- teste-frontend/src/components/BlogPost/index.jsx
+++ teste-frontend/src/components/BlogPost/index.jsx
@@ -1,10 +1,30 @@
+import { useState, useEffect } from "react";
 import "./blogPost.css";
 
 export function BlogPost() {
+  const [posts, setPosts] = useState([]);
+
+  const api = "https://jsonplaceholder.typicode.com/posts";
+  useEffect(() => {
+    fetch(api)
+      .then((response) => response.json())
+      .then((data) => setPosts(data));
+  }, []);
+
   return (
-    <div className="post-box">
-      <h3>Título</h3>
-      <p>Postagem</p>
+    <div>
+      <ul>
+        {posts.map((post) => {
+          return (
+            <li key={post.title} className="posts-list">
+              <div className="post-box">
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
