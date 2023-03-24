d1e29f11d27bf578157733ca74eb4f80f9567140
AdsonVilla <adson23_@hotmail.com>
2023-03-24 11:39:21 -0300

    feat: Adicionar a biblioteca moment.js para pegar a data atual

--- teste-frontend/src/pages/Comments/index.jsx
+++ teste-frontend/src/pages/Comments/index.jsx
@@ -1,14 +1,17 @@
 import { Footer } from "../../components/Footer";
 import { Navbar } from "../../components/Navbar";
 import { PostComments } from "../../components/PostComments";
+import moment from "moment";
 import "./comments.css";
 
 function Comments() {
+  const date = new Date();
+  const formatDate = moment(date).format("DD/MM/YYYY");
   return (
     <div>
       <Navbar />
       <header className="header">
-        <span>22/03/2023</span>
+        <span>{formatDate}</span>
         <h1>Comentários</h1>
       </header>
       <PostComments />
--- teste-frontend/src/pages/Home/index.jsx
+++ teste-frontend/src/pages/Home/index.jsx
@@ -3,14 +3,17 @@ import { BlogPost } from "../../components/BlogPost";
 import { ContactUs } from "../../components/ContactUs";
 import { Footer } from "../../components/Footer";
 import { Navbar } from "../../components/Navbar";
+import moment from "moment";
 import "./home.css";
 
 function Home() {
+  const date = new Date();
+  const formatDate = moment(date).format("DD/MM/YYYY");
   return (
     <div id="top">
       <Navbar />
       <header className="header">
-        <span>23/03/2022</span>
+        <span>{formatDate}</span>
         <h1>Posts recentes</h1>
       </header>
       <div className="posts-container">
